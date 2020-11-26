699fb800b7eea8867a67169d47892dc2b74d870e
nikhilleo <ramrakhyani94@gmail.com>
2020-11-27 00:04:45 +0530

    Nik

--- login-page-frontend/src/MyForm/Login.js
+++ login-page-frontend/src/MyForm/Login.js
@@ -1,28 +1,42 @@
-import React from "react";
-import "./Login.css";
-import image1 from "../Assests/image1.jpg";
-import { Link } from "react-router-dom";
+import React, { useState} from "react";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
 
-function login() {
-  return (
-    <>
-      <div className="root">
-        <div className="root__img">
-          <img src={image1} />
-        </div>
-        <div className="root__form">
-          <p className="sub__para p">*E-MAIL</p>
-          <input type="text" className="sub__inputs" />
-          <p className="p">*PASSWORD</p>
-          <input type="text" className="third__inputs" />
-          <button>REGISTER</button>
-          <Link to="/">
-            <p id="alread">REGISTER?</p>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
-}
+export default function Login() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState();
+    const history = useHistory();
 
-export default login;
+    const submit = async (event) => {
+        event.preventDefault();
+        try {
+            const loginUser = { email, password };
+            const loginRes = await Axios.post(
+                "http://localhost:8080/login",
+                loginUser
+            );
+              console.log(loginRes);
+            localStorage.setItem("auth-token", loginRes.data.token);
+            history.push("/");
+        } catch (err) {
+            console.log(err.message)
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={submit}>
+                <div class="form-group">
+                    <label >Email address</label>
+                    <input onChange={(event) => setEmail(event.target.value)} type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required></input>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input onChange={(event) => setPassword(event.target.value)} type="password" name='password' class="form-control" id="exampleInputPassword1" required></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+)}    
