c1e871678ed66337e08d325ba3dca834f135e0ec
nikhilleo <ramrakhyani94@gmail.com>
2020-12-07 13:39:35 +0530

    Booking Model

---
+++ Login-prathmesh/backend/models/booking.js
@@ -0,0 +1,33 @@
+
+
+const mongoose = require('mongoose');
+const Camper = require("../models/camper");
+
+const bookingSchema = new mongoose.Schema({
+    a_details:{
+        type:Array,
+        required:true
+    },
+    total_amount:{
+        type:String,
+        trim:true,
+        default:""
+    },
+    camper_details:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Camper"
+    },
+    payment_id:{
+        type:String,
+        trim:true,
+        unique:true
+    },
+    payment_status:{
+        type:String,
+        trim:true,
+        default:"Pending"
+    }
+},{timestamps:true});
+
+const Bookings = mongoose.model("Bookings",bookingSchema);
+module.exports = Bookings;
