4af546fc29175dab9363810de33f4b4ce42d1c76
swapnilshinde123 <swapnil6901@gmail.com>
2020-12-08 00:03:57 +0530

    camp

--- Login-prathmesh/frontend/src/HomePage/index.js
+++ Login-prathmesh/frontend/src/HomePage/index.js
@@ -54,174 +54,185 @@ function Index(props) {
 
   console.log(campDetails);
   return (
-    <div>
-      <Grid container>
-        <Grid item xs={2}></Grid>
-        <Grid item xs={10}>
-          <img src={backImg1} />
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid container>
-          <Grid item xs={2}></Grid>
-          <Grid item sm={3} xs={12}>
-            <h1 style={{ fontFamily: "sans-serif" }}>Recent Camps</h1>
-          </Grid>
-        </Grid>
-        <Grid container xs={12} style={{ visibility: "hidden" }}>
-          .
-        </Grid>
-        <Grid container>
-          <Grid item xs={2}></Grid>
-          {campDetails.map((item, key) => (
-            <>
-              <Grid container xs={1}>
-                <img src={item.camp_images[0]} className="photos" />
-                <Grid container xs={12} style={{ visibility: "hidden" }}>
-                  .
-                </Grid>
-                <Grid item sm={11} xs={10}>
-                  <p
-                    style={{
-                      overflow: "hidden",
-                      whiteSpace: "no-wrap",
-                      textOverflow: "ellipsis",
-                      fontWeight: "bolder",
-                    }}
-                  >
-                    {item.camp_name}
-                  </p>
-                </Grid>
-                <Grid item sm={1} xs={10}>
-                  <Button
-                    onClick={() => handleClick(item.camp_name)}
-                    variant="contained"
-                    color="secondary"
-                  >
-                    Details
-                  </Button>
-                </Grid>
-                <Grid item sm={11} xs={10}>
-                  <p
-                    style={{
-                      overflow: "hidden",
-                      whiteSpace: "no-wrap",
-                      textOverflow: "ellipsis",
-                    }}
-                  >
-                    {item.camp_location}
-                  </p>
-                </Grid>
-              </Grid>
-              <Grid item xs={1}></Grid>
-            </>
-          ))}
-        </Grid>
-      </Grid>
-      <Grid container xs={12} style={{ visibility: "hidden" }}>
-        .
-      </Grid>
-      <Grid container>
-        <Grid item xs={2}></Grid>
-        <Grid item sm={3} xs={12}>
-          <h1 style={{ fontFamily: "sans-serif" }}>POPULAR LOCATIONS</h1>
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid container>
-          <Grid item xs={2}></Grid>
-          <Grid item sm={2} xs={10}>
-            <img
-              className="photos"
-              src="https://images.pexels.com/photos/1459505/pexels-photo-1459505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            ></img>
-          </Grid>
-          <Grid item sm={2} xs={10}>
-            <img
-              className="photos"
-              src="https://images.pexels.com/photos/1459505/pexels-photo-1459505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            ></img>
-          </Grid>
-          <Grid item sm={2} xs={10}>
-            <img
-              className="photos"
-              src="https://images.pexels.com/photos/1459505/pexels-photo-1459505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            ></img>
-          </Grid>
-          <Grid item sm={2} xs={10}>
-            <img
-              className="photos"
-              src="https://images.pexels.com/photos/1459505/pexels-photo-1459505.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            ></img>
-          </Grid>
-        </Grid>
-        <Grid container xs={12} style={{ visibility: "hidden" }}>
-          .
-        </Grid>
-        <Grid container>
-          <Grid item xs={2}></Grid>
-          <Grid item sm={1} xs={10}>
-            <p style={{ fontWeight: "bolder" }}>Camp name</p>
-          </Grid>
-          <Grid item sm={1} xs={10}>
-            <Button variant="contained" color="secondary">
-              Details
-            </Button>
-          </Grid>
-          <Grid item sm={1} xs={10}>
-            <p style={{ fontWeight: "bolder" }}>Camp name</p>
-          </Grid>
-          <Grid item sm={1} xs={10}>
-            <Button variant="contained" color="secondary">
-              Details
-            </Button>
-          </Grid>
-          <Grid item sm={1} xs={10}>
-            <p style={{ fontWeight: "bolder" }}>Camp name</p>
-          </Grid>
-          <Grid item sm={1} xs={10}>
-            <Button variant="contained" color="secondary">
-              Details
-            </Button>
-          </Grid>
-          <Grid item sm={1} xs={10}>
-            <p style={{ fontWeight: "bolder" }}>Camp name</p>
-          </Grid>
-          <Grid item sm={1} xs={10}>
-            <Button variant="contained" color="secondary">
-              Details
-            </Button>
-          </Grid>
-        </Grid>
-        <Grid container>
-          <Grid item xs={2}></Grid>
-          <Grid item sm={2} xs={10}>
-            <p style={{ fontSize: 13 }}>Camp Lcation</p>
-          </Grid>
-          <Grid item sm={2} xs={10}>
-            <p style={{ fontSize: 13 }}>Camp Location</p>
-          </Grid>
-          <Grid item sm={2} xs={10}>
-            <p style={{ fontSize: 13 }}>Camp Location</p>
-          </Grid>
-          <Grid item sm={2} xs={10}>
-            <p style={{ fontSize: 13 }}>Camp Location</p>
-          </Grid>
-        </Grid>
-        <Grid container xs={12} style={{ visibility: "hidden" }}>
-          .
-        </Grid>
-        <Grid container xs={12} style={{ visibility: "hidden" }}>
-          .
-        </Grid>
-        <Grid container xs={12} style={{ visibility: "hidden" }}>
-          .
-        </Grid>
-      </Grid>
-      <footer>
+    <div className="main">
+    <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+  
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-2" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-2" data-slide-to="1"></li>
+    <li data-target="#carousel-example-2" data-slide-to="2"></li>
+  </ol>
+ 
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <div class="view">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+          alt="First slide"/>
+        <div class="mask rgba-black-light"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Light mask</h3>
+        <p>First text</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+ 
+      <div class="view">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg"
+          alt="Second slide"/>
+        <div class="mask rgba-black-strong"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Strong mask</h3>
+        <p>Secondary text</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      
+      <div class="view">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).jpg"
+          alt="Third slide"/>
+        <div class="mask rgba-black-slight"></div>
+      </div>
+      <div class="carousel-caption">
+        <h3 class="h3-responsive">Slight mask</h3>
+        <p>Third text</p>
+      </div>
+    </div>
+  </div>
+</div>
+  
+  
+
+
+
+<div class="container-fluid mt-5" >
+	<div class="row">
+		<div class="col-sm-12">
+			<div id="inam" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						 <div class="container">
+						 	<div class="row">
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card">
+						 				<img src="https://images.unsplash.com/photo-1529385101576-4e03aae38ffc?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzF8fGNhbXBpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top"/>
+						 				<div class="card-body">
+						 					<h4 class="card-title">“Life is better by the camp fire”</h4>
+						 					<p class="card-text">Camping is an outdoor activity involving overnight stays away from home in a shelter, such as a tent or a recreational vehicle.</p>
+						 					<button type="button" class="btn btn-warning">Read More</button>
+						 					
+						 				</div>
+						 				
+						 			</div>
+						 			
+						 		</div>
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" >
+						 				<img src="https://images.unsplash.com/photo-1464547323744-4edd0cd0c746?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjN8fGNhbXBpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top"/>
+						 				<div class="card-body">
+						 					<h4 class="card-title">“Life is better by the camp fire”</h4>
+						 					<p class="card-text">Camping is an outdoor activity involving overnight stays away from home in a shelter, such as a tent or a recreational vehicle.</p>
+						 					<button type="button" class="btn btn-warning">Read More</button>
+						 					
+						 				</div>
+						 				
+						 			</div>
+						 			
+						 		</div>
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" >
+						 				<img src="https://images.unsplash.com/photo-1511993807578-701168605ad3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjV8fGNhbXBpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top"/>
+						 				<div class="card-body">
+						 					<h4 class="card-title">“Life is better by the camp fire”</h4>
+						 					<p class="card-text">Camping is an outdoor activity involving overnight stays away from home in a shelter, such as a tent or a recreational vehicle.</p>
+						 					<button type="button" class="btn btn-warning">Read More</button>
+						 					
+						 				</div>
+						 				
+						 			</div>
+						 			
+						 		</div>
+						 		
+						 	</div>
+						 	
+						 </div>
+
+						
+					</div>
+					<div class="carousel-item">
+						 <div class="container">
+						 	<div class="row">
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card ">
+						 				<img src="https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FtcGluZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top"/>
+						 				<div class="card-body">
+						 					<h4 class="card-title">“Life is better by the camp fire”</h4>
+						 					<p class="card-text">Camping is an outdoor activity involving overnight stays away from home in a shelter, such as a tent or a recreational vehicle.</p>
+						 					<button type="button" class="btn btn-warning">Read More</button>
+						 					
+						 				</div>
+						 				
+						 			</div>
+						 			
+						 		</div>
+						 		<div class="col-sm-12 col-lg-4">
+						 			<div class="card" >
+						 				<img src="https://images.unsplash.com/photo-1530541930197-ff16ac917b0e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8Y2FtcGluZ3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top"/>
+						 				<div class="card-body">
+						 					<h4 class="card-title">“Life is better by the camp fire”</h4>
+						 					<p class="card-text">Camping is an outdoor activity involving overnight stays away from home in a shelter, such as a tent or a recreational vehicle.</p>
+						 					<button type="button" class="btn btn-warning">Read More</button>
+						 					
+						 				</div>
+						 				
+						 			</div>
+						 			
+						 		</div>
+						 		<div class="col-sm-12 col-lg-4 ">
+						 			<div class="card ">
+						 				<img src="https://images.unsplash.com/photo-1525811902-f2342640856e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGNhbXBpbmd8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top"/>
+						 				<div class="card-body">
+						 					<h4 class="card-title">“Life is better by the camp fire”</h4>
+						 					<p class="card-text">Camping is an outdoor activity involving overnight stays away from home in a shelter, such as a tent or a recreational vehicle.</p>
+						 					<button type="button" class="btn btn-warning">Read More</button>
+						 					
+						 				</div>
+						 				
+						 			</div>
+						 			
+						 		</div>
+						 		
+						 	</div>
+						 	
+						 </div>
+
+						
+					</div>
+					
+				</div>
+				<a href="#inam" class="carousel-control-prev "  data-slide="prev">
+					<span class="carousel-control-prev-icon mr-5"></span>
+				</a>
+				<a href="#inam" class="carousel-control-next" data-slide="next">
+					<span class="carousel-control-next-icon ml-5 "></span>
+				</a>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+	
+    </div>
+   <div className="mt-5">
+    <footer>
         <Footer />
       </footer>
-    </div>
+   </div>
+      </div>
+    
+
   );
 }
 
@@ -243,3 +254,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Index);
+
